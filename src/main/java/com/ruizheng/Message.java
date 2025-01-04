7284e866a267ad74f30a1450cddeb7c7614f0142
Rui Zheng <zhengrui315@gmail.com>
2025-01-04 01:02:10 -0800

    Implemented basic chat using WebSocket.

---
+++ src/main/java/com/ruizheng/Message.java
@@ -0,0 +1,24 @@
+package com.ruizheng;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+    private String sender;
+    private String receiver;
+    private String content;
+    private String time;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT, JOIN, LEAVE
+    }
+
+
+}
